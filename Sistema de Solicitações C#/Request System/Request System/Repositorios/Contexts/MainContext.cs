0bf37b7c6c321cf77db3b9a1c6e47a0b642b5158
Gustavo Marin Suppi <gustavomarinsuppi@gmail.com>
2019-06-29 20:28:57 -0300

    Criação do contexto pelo banco de dados informado na connection string. Adição da configuração de usuários

---
+++ Sistema de Solicitações C#/Request System/Request System/Repositorios/Contexts/MainContext.cs
@@ -0,0 +1,24 @@
+﻿using Request_System.Repositorios.Configurations;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.Entity;
+
+namespace Request_System.Repositorios.Contexts
+{
+    //https://www.entityframeworktutorial.net/code-first/move-configurations-to-seperate-class-in-code-first.aspx
+
+    public class MainContext : DbContext
+    {
+        public List<User> Users { get; set; }
+
+        public MainContext() : base(ConfigurationManager.ConnectionStrings["CS"].ConnectionString)
+        {
+
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Configurations.Add(new UserConfiguration());
+        }
+    }
+}
